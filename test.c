eb9e83840447dda0ad7a922951fbd7c7bf30dbee
ngoctan3006 <ngoctana1k2014@gmail.com>
2021-12-22 15:51:50 +0700

    add test.c

--- test.c
+++ test.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
+#include <ctype.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     double x;
@@ -7,7 +12,14 @@ typedef struct {
     char name[51];
 } LOC;
 
+typedef struct aPlace {
+    int hour, min;
+    char locName[255];
+    struct aPlace *next;
+} PLACE;
+
 void listF1Covid(LOC *listLoc, int n, double x, double y, double d);
+int needMedicalReport(PLACE *visitedPlace, char *reportLocName, int reportHour, int reportMin);
 
 int main() {
     return 0;
@@ -20,4 +32,16 @@ void listF1Covid(LOC *listLoc, int n, double x1, double y1, double d) {
             printf("%s\n", listLoc[i].name);
         }
     }
+}
+
+int needMedicalReport(PLACE *visitedPlace, char *reportLocName, int reportHour, int reportMin) {
+    PLACE *temp = visitedPlace;
+    while (temp != NULL) {
+        if (strcmp(temp->locName, reportLocName) == 0) {
+            if (((temp->hour == reportHour) && (temp->min >= reportMin)) || (temp->hour > reportHour))
+                return 1;
+        }
+        temp = temp->next;
+    }
+    return 0;
 }
